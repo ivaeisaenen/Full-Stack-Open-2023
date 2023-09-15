9c29a0e3e0c0cb510d0d0d099f5d8948a72f55a3
unknown <ivaeisaenen@gmail.com>
2023-09-15 07:18:05 +0300

    Part1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,19 +13,38 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [voted, setVoted] = useState(Array(anecdotes.length).fill(0))
 
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
+  const setToVoted = (newVote)=> {
+    console.log('newVote = ', newVote)
+    console.log('voted = ', voted)
+    const newVoted = [...voted]
+    newVoted[newVote] += 1
+    console.log('newVoted = ', newVoted)
+    setVoted(newVoted)
+    console.log('voted = ', voted)
+  }
+
+const setToSelected = (randomInt) => {
+  console.log('selected = ', selected)
+  setSelected(randomInt)
+  console.log('selected = ', selected)
+}
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
-        <button onClick={() => setSelected(getRandomInt(anecdotes.length))}>
-          next anectode
-        </button>
-
+      <button onClick={() => setToVoted(selected)}>
+        vote
+      </button>
+      <button onClick={() => setToSelected(getRandomInt(anecdotes.length))}>
+        next anectode
+      </button>
     </div>
   )
 }
